a4a2beaab20a980c24230403dad066c899a3f134
Chiraj <chiraj15>
2020-08-06 22:46:01 +0530

    Commit on 6-8-20

---
+++ 1z0-808/src/Question_09.java
@@ -0,0 +1,50 @@
+/*
+ * NOT COMPLETED 
+ * Topic : Working with Selected Classes from the Java API
+ * Code: 05
+ * 
+ */
+public class Question_09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[] = {1,2,3,4,5};
+		
+//		for(???) {
+//			System.out.print(a[i]);
+//		}
+
+	}
+
+}
+
+
+/*
+ * Which option will replace the ??? for the following code to print 135 ?
+ * 
+ * a)
+ * int i=0 ; i<=4; i++
+ * 
+ * b)
+ * int i=1 ; i<=5; i=i+2
+ *  
+ * c)
+ * int i=1 ; i<=5; i+=1 
+ * 
+ * d)
+ * int i=0 ; i<5; i=i+2
+ * 
+ * Answer : 
+ * d)
+ *int i=0 ; i<5; i=i+2
+ * 
+ * Explanation:
+ * The index of an array in java starts from zero.
+ * Therefore the following code fragment compiles to produce the output 135.
+ * 
+ * Additional Explanation: 
+ * 
+ */
+
+
